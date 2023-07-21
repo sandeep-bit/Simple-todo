839ea4003cbb88e88e78bde96ebe41c1e44e48c9
rohan <ping.techyrohan@gmail.com>
2023-07-21 23:56:35 +0545

    fetch logic implemented

--- frontend/src/components/Button.jsx
+++ frontend/src/components/Button.jsx
@@ -1,6 +1,6 @@
 import styles from './Button.module.css'
 function Button({children , onClick}) {
-    return <button className={styles.btn}>{children}</button>
+    return <button className={styles.btn} onClick={onClick}>{children}</button>
 }
 
 export default Button;
